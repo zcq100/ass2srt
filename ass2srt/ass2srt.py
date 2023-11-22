e4ca08f441c83835d4c6d5b5969b609a63c6e351
Antonin Dudermel <antonin.dudermel@caramail.fr>
2023-11-22 19:21:22 +0100

    fix removing of text after a comma

--- ass2srt/ass2srt.py
+++ ass2srt/ass2srt.py
@@ -27,6 +27,7 @@ class Ass2srt:
                 node = line.split(",")
                 node[1] = timefmt(node[1])
                 node[2] = timefmt(node[2])
+                node[9] = ",".join(node[9:])
                 node[9] = re.sub(r'{[^}]*}', "", node[9]).strip()
                 node[9] = re.sub(r'\\N', "\n", node[9])
                 self.nodes.append(node)
